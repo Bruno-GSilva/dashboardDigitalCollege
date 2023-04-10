ae566948fe9c5eef950613829bf7e4c6130fea51
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-04-10 15:47:56 -0300

    ajustes

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 const darkmode = document.getElementById('darkmode')
 const burger = document.getElementById('burger')
 const a = document.querySelectorAll('a')
+const burgerColor = document.querySelectorAll('#header > div:nth-child(3) > label > span')
 
 darkmode.addEventListener('click', () => {
     darkmode.classList.toggle('active')
     header.classList.toggle('active')
     ul.classList.toggle('dark')
     a.forEach((item)=> item.classList.toggle('active'))
+    burgerColor.forEach((item)=> item.classList.toggle('active'))
 })
 burger.addEventListener('click', () => {
     ul.classList.toggle('active')
