77fffc1f722f16815c467cee2017bde91fad8b3e
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-04-10 14:43:12 -0300

    darkmode

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 const darkmode = document.getElementById('darkmode')
+const burger = document.getElementById('burger')
+const a = document.querySelectorAll('a')
 
 darkmode.addEventListener('click', () => {
     darkmode.classList.toggle('active')
     header.classList.toggle('active')
+    ul.classList.toggle('dark')
+    a.forEach((item)=> item.classList.toggle('active'))
+})
+burger.addEventListener('click', () => {
+    ul.classList.toggle('active')
 })
