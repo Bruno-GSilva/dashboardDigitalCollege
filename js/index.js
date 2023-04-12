0467c69f8e60d3eb7e4855da9de9b330a0a68991
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-04-12 10:04:12 -0300

    config

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
-const darkmode = document.getElementById('darkmode')
-const burger = document.getElementById('burger')
-const a = document.querySelectorAll('a')
-const burgerColor = document.querySelectorAll('#header > div:nth-child(3) > label > span')
+const darkmode = document.getElementById('darkmode');
+const burger = document.getElementById('burger');
+const a = document.querySelectorAll('a');
+const burgerColor = document.querySelectorAll('#header > div:nth-child(3) > label > span');
+const header = document.querySelector('header');
+const ul = document.querySelector('ul');
 
 darkmode.addEventListener('click', () => {
-    darkmode.classList.toggle('active')
-    header.classList.toggle('active')
-    ul.classList.toggle('dark')
-    a.forEach((item)=> item.classList.toggle('active'))
-    burgerColor.forEach((item)=> item.classList.toggle('active'))
-})
+  darkmode.classList.toggle('active');
+  localStorage.setItem('lightMode', darkmode.classList.contains('active'));
+  header.classList.toggle('active');
+  ul.classList.toggle('dark');
+  a.forEach((item) => item.classList.toggle('active'));
+  burgerColor.forEach((item) => item.classList.toggle('active'));
+});
+
 burger.addEventListener('click', () => {
-    ul.classList.toggle('active')
-})
+  ul.classList.toggle('active');
+});
+
+window.addEventListener('load', () => {
+  if (localStorage.getItem('lightMode') === 'true') {
+    darkmode.click();
+  }
+});
